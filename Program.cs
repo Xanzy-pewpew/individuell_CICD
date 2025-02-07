be5609dd0376171dde884f006b80259deda62eb7
Kevin BJ <kevin.b.jonsson@hotmail.com>
2025-02-07 09:45:21 +0100

    update welcome info

--- Program.cs
+++ Program.cs
@@ -3,7 +3,9 @@ var app = builder.Build();
 
 var cipherService = new CICD_cipher.CipherService();
 
-app.MapGet("/", () => "Welcome to API for Caesar Cipher!");
+app.MapGet("/", () => "Welcome to API for Caesar Cipher!\n\n +" +
+"For encrypting use: /encrypt?input= <text>\n\n" +
+"For decrypting use: /decrypt?dinput= <text>");
 
 //Endpoint for encrypting the input
 app.MapGet("/encrypt", (string input) =>
